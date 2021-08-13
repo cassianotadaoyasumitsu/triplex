f08deb77f246481ca5a72939ba5b6986e1c645ed
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-08-14 00:13:27 +0900

    master

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,17 +2,23 @@
 
 int main()
 {
+  // How to add a comment!
+  /*
+  How to add multi line comment
+  */
   std::cout << "You are a secret agent breaking into a secure server room...";
   std::cout << std::endl;
   std::cout << "You need to enter the correct codes to continue...";
 
-  int a = 4;
-  int b = 3;
-  int c = 2;
+  // Declare numbers
+  const int a = 4;
+  const int b = 3;
+  const int c = 2;
 
-  int sum = a + b + c;
-  int product = a * b * c;
+  const int sum = a + b + c;
+  const int product = a * b * c;
 
+  // Print
   std::cout << std::endl;
   std::cout << sum << std::endl;
   std::cout << product << std::endl;
