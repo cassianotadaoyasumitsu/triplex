05726550033b0dfb3e4a8e730c0d197ec4b20ccf
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-08-13 23:45:08 +0900

    string

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,5 +2,8 @@
 
 int main()
 {
+  std::cout << "You are a secret agent breaking into a secure server room...";
+  std::cout << std::endl;
+  std::cout << "You need to enter the correct codes to continue...";
   return 0;
 }
