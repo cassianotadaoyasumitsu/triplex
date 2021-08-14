f43a0e94ce8b3a68dc257a05172448dd1711a150
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-08-14 18:46:19 +0900

    master

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,36 +1,97 @@
 #include <iostream>
+#include <ctime>
 
-int main()
+void PrintIntroduction(int Difficulty)
 {
+  std::cout << "  ▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄ \n";
+  std::cout << " ▐░░░░░░░░░▌ ▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌\n";
+  std::cout << "▐░█░█▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌\n";
+  std::cout << "▐░▌▐░▌    ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌\n";
+  std::cout << "▐░▌ ▐░▌   ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌          ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌\n";
+  std::cout << "▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░▌          ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌\n";
+  std::cout << "▐░▌   ▐░▌ ▐░▌▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀█░▌▐░▌          ▐░▌       ▐░▌▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌\n";
+  std::cout << "▐░▌    ▐░▌▐░▌▐░▌       ▐░▌          ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌     ▐░▌  ▐░▌           ▀ \n";
+  std::cout << "▐░█▄▄▄▄▄█░█░▌▐░█▄▄▄▄▄▄▄█░▌ ▄▄▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░▌      ▐░▌ ▐░█▄▄▄▄▄▄▄▄▄  ▄ \n";
+  std::cout << " ▐░░░░░░░░░▌ ▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌\n";
+  std::cout << "  ▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀ \n\n";
+  std::cout << "You are a secret agent breaking into a level " << Difficulty;
+  std::cout << " secure server room...\nYou need to enter the correct codes to continue...\n\n";
+}
+
+bool PlayGame(int Difficulty)
+{
+  PrintIntroduction(Difficulty);
+
   // How to add a comment!
   /*
   How to add multi line comment
   */
-  std::cout << "You are a secret agent breaking into a secure server room...";
-  std::cout << std::endl;
-  std::cout << "You need to enter the correct codes to continue..." << std::endl;
+
 
   // Declare numbers
-  const int CodeA = 4;
-  const int CodeB = 3;
-  const int CodeC = 2;
+  const int CodeA = rand() % Difficulty + Difficulty;
+  const int CodeB = rand() % Difficulty + Difficulty;
+  const int CodeC = rand() % Difficulty + Difficulty;
 
   const int CodeSum = CodeA + CodeB + CodeC;
   const int CodeProduct = CodeA * CodeB * CodeC;
 
   // Print
-  std::cout << std::endl;
-  std::cout << "There are 3 numbers in the code" << std::endl;
-  std::cout << "The codes add-up to: " << CodeSum << std::endl;
-  std::cout << "The codes multiply to give: " << CodeProduct << std::endl;
+  std::cout << "There are 3 numbers in the code\n";
+  std::cout << "\nThe codes add-up to: " << CodeSum;
+  std::cout << "\nThe codes multiply to give: " << CodeProduct << "\n";
+  std::cout << "Do you have only 3 chances!\n";
 
+  // Store player guess
   int GuessA, GuessB, GuessC;
+  std::cout << "1 Guess: ";
   std::cin >> GuessA;
+  std::cout << "2 Guess: ";
   std::cin >> GuessB;
+  std::cout << "3 Guess: ";
   std::cin >> GuessC;
 
   int GuessSum  = GuessA + GuessB + GuessC;
   int GuessProduct = GuessA * GuessB * GuessC;
 
+  // if statement
+  if (GuessSum == CodeSum && GuessProduct == GuessProduct)
+  {
+    if (Difficulty <= 4)
+    {
+    std::cout << "\nYou win! Go to next Level!\n\n";
+    }
+    return true;
+  }
+  else
+  {
+    std::cout << "\nHahahahaha, Try again! LOSER!\n\n";
+    return false;
+  }
+}
+
+int main()
+{
+  srand(time(NULL));
+  
+  int LevelDifficulty = 1;
+
+  const int MaxLevel = 5;
+
+  while (LevelDifficulty <= MaxLevel) // Loop the game until all levels
+  {
+    bool bLevelComplete = PlayGame(LevelDifficulty);
+    // Clear any typo
+    std::cin.clear();
+    // Discards the buffer
+    std::cin.ignore();
+
+    if (bLevelComplete)
+    {
+      ++LevelDifficulty;
+    }
+
+  }
+  std::cout << "\n\nBastard you are free!\n";
   return 0;
 }
